e62346f3a3927855df91cee314d405583a118562
Mathias Buus <mathiasbuus@gmail.com>
2012-05-14 00:37:24 +0200

    added browser request

--- app/module.js
+++ app/module.js
@@ -2,7 +2,8 @@ var request = require('browser-request');
 var name = $('script').data('name');
 
 $('.approve').on('click', function() {
-	request.post('/approves/'+name, {
+	request.post({
+		url: '/approves/'+name,
 		json: true
 	}, function(err, response) {
 		console.log('her nu...')
