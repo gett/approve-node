da167c5de4117f082e575fc9ff2ce813e09bc316
Tobias Baunbaek <t@ge.tt>
2012-05-13 23:25:37 +0200

    Base.css

--- app/index.js
+++ app/index.js
@@ -6,4 +6,12 @@ $(function() {
 		window.location = '/search/'+$(this).val();	
 	});
 
+	$('.help-us').on('click', '.read-more', function() {
+		$(this).text('Read less').removeClass('read-more').addClass('read-less');
+		$('.more-info').slideDown();
+	});
+	$('.help-us').on('click', '.read-less', function() {
+		$(this).text('Read more').removeClass('read-less').addClass('read-more');
+		$('.more-info').slideUp();
+	});
 });
