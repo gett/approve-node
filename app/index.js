1b3e7794aa39fecf652ced40743de0a51e9bbde4
Mathias Buus <mathiasbuus@gmail.com>
2012-05-13 23:28:30 +0200

    added auto capture

--- app/index.js
+++ app/index.js
@@ -1,4 +1,7 @@
 $(function() {
+	var LATIN = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789'.split('').map(function(ch) {
+		return ch.charCodeAt(0);
+	});
 
 	$('.search').on('keydown', function(e) {
 		if (e.keyCode !== 13) return;
@@ -6,4 +9,9 @@ $(function() {
 		window.location = '/search/'+$(this).val();	
 	});
 
+	$(document).on('keydown', function(e) {
+		if (LATIN.indexOf(e.keyCode) < 0) return;
+
+		$('.search').focus();
+	});
 });
