e36e3f22071027626b939fb47230425a563dcb4b
Tobias Baunbaek <t@ge.tt>
2012-05-13 19:24:16 +0200

    Login, logout, secure cookie, database flow, and templating all working

--- server.js
+++ server.js
@@ -1,17 +1,19 @@
 var pejs = require('pejs');
 var root = require('root');
-var less = require('connect-lesscss');
 var rex = require('rex');
-var request = require('request');
 var common = require('common');
-var qs = require('querystring');
 var db = require('mongojs').connect('approve:qweqwe@staff.mongohq.com:10075/approve-node', ['users']);
+var less = require('connect-lesscss');
+var request = require('request');
+var qs = require('querystring');
+var crypto = require('crypto');
 
 var app = root();
 var template = pejs();
 
 var GITHUB_ID = 'e411408f92365f7bbf0a';
 var GITHUB_SECRET = '6795299bd3f3c24e7af6275f4483cd11e892c152';
+var SECRET = 'slfjasfo87234ifgkj';
 
 app.use(root.json);
 app.use(root.query);
@@ -21,6 +23,31 @@ app.use('/index.js', rex('./app/index.js', {
 		jQuery: 'http://ajax.googleapis.com/ajax/libs/jquery/1.7.2/jquery.min.js'
 	}
 }));
+
+app.getter('request.id', function() {
+	if (this._id) {
+		return this._id;
+	}
+
+	var cookie = this.headers.cookie;
+
+	cookie = cookie && (cookie.split('id=')[1] || '').split(';')[0];
+
+	if (!cookie) {
+		return null;
+	}
+
+	var id = cookie.split('.')[0];
+	var date = cookie.split('.')[1];
+	var signature = cookie.split('.')[2];
+
+	if (crypto.createHmac('sha256', SECRET).update(id+'.'+date).digest('hex') !== signature) {
+		return null;
+	}
+
+	this._id = parseInt(id, 10);
+	return this._id;
+});
 
 app.fn('response.render', function(filename, options) {
 	var self = this;
@@ -48,13 +75,45 @@ app.fn('response.send', function(statusCode, message) {
 	this.setHeader('content-length', Buffer.byteLength(message));
 	this.end(message);
 });
+app.fn('response.redirect', function(url) {
+	this.statusCode = 307;
 
-app.get('/', function(req, res) {
-	res.render('app/index.html');
+	if (url.charAt(0) === '/') {
+		url = 'http://'+this.request.headers.host+url;
+	}
+
+	this.setHeader('location', url);
+	this.end();
+});
+
+app.get('/', function(req, res, onerror) {
+	if (!req.id) {
+		res.render('app/index.html');
+		return;
+	}
+
+	common.step([
+		function(next) {
+			db.users.findOne({id:req.id}, next);
+		},
+		function(user) {
+			res.render('app/home.html', user);
+		}
+	], onerror);
+});
+
+app.get('/modules/{name}', function(req, res) {
+
+});
+
+app.get('/signout', function(req, res) {
+	res.setHeader('set-cookie', 'id=0; expires Thu, 01 Jan 1970 00:00:00 GMT');
+	res.redirect('/');
 });
 
 app.get('/authorized', function(req, res, onerror) {
 	var code = req.query.code;
+	var cookie;
 
 	if (!code) {
 		res.send(400, 'Need token');
@@ -85,18 +144,21 @@ app.get('/authorized', function(req, res, onerror) {
 			}, next);
 		},
 		function(response, next) {
-			var user = response.body;
-
 			if (response.statusCode !== 200) {
 				res.send(response.statusCode, 'Error fetching user data');
 				return;
 			}
 
+			var user = response.body;
+
+			cookie = user.id+'.'+Date.now();
+			cookie += '.'+crypto.createHmac('sha256', SECRET).update(cookie).digest('hex');
+
 			db.users.update({id:user.id}, {$set:user}, {upsert:true}, next);
 		},
 		function() {
-			res.send(user);
-			console.log('returned');
+			res.setHeader('set-cookie', 'id='+cookie+'; expires='+new Date(Date.now() + 14*24*60*60*1000).toUTCString());
+			res.redirect('/');
 		}
 	], onerror);
 });
